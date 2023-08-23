875349843708b98f0983bba9a87074b97a587af7
Yan-Daojiang <yan_daojiang@163.com>
2023-08-24 01:24:34 +0800

    🎈 perf(Install): pre-isntall
    Integrate some operations into pre-install.sh

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Dotfiles [![Build Status](https://github.com/Yan-Daojiang/dotfiles/workflows/CI/badge.svg)](https://github.com/Yan-Daojiang/dotfiles/actions?query=workflow%3ACI)
 
+> 就像木匠定制他们的工作空间一样，开发人员也应该定制自己的 Shell. —— 《程序员修炼之道》
+
 # Getting Started
 > Please ensure that you have a good network environment, especially when you are in mainland China.
 ```
@@ -14,13 +16,10 @@ git clone https://github.com/Yan-Daojiang/dotfiles.git
 docker build -t my-dotfiles -f ./docker/Dockerfile .
 docker run --rm -it my-dotfiles
 ```
-
 </details>
 
 ## Install
 ``` 
 # current directory is dotfiles
-touch ./zsh/private.zsh ./git/gitconfig_local  # create local config files
 ./install
 ```
-
--- pre-install.sh
+++ pre-install.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
+# Set Zsh as the default shell
+if [[ "$SHELL" != *"/zsh" ]]; then
+    chsh -s "$(command -v zsh)"
+    echo "Changed default shell to Zsh. Please log out and log back in for the changes to take effect."
+else
+    echo "Zsh is already the default shell."
+fi
 
+
+# Set Zsh as the default shell
+if [[ "$SHELL" != *"/zsh" ]]; then
+    chsh -s "$(command -v zsh)"
+    echo "Changed default shell to Zsh. Please log out and log back in for the changes to take effect."
+else
+    echo "Zsh is already the default shell."
+fi
+
+# Check if local config directories exist
+if [ ! -d "./zsh" ]; then
+    mkdir "./zsh"
+fi
+if [ ! -d "./git" ]; then
+    mkdir "./git"
+fi
+
+if [ -f ~/.zshrc ]; then
+  echo 'Backing up existing zshrc'
+  mv ~/.zshrc ~/.zshrc.bak
+fi
+
+# create local config files
+touch ./zsh/private.zsh 
+touch ./git/gitconfig_local
 # Change default shell
 if [! $0 = "-zsh"]; then
   echo 'Changing default shell to zsh'
